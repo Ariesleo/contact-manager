d11bd7c1657adfa0aedbbb7d4a3a559c7ab35396
Murari Gupta <alexgupta2367@gmail.com>
2022-04-04 22:58:14 +0545

    installing the jwttoken and generating the token once signin

--- Backend/src/models/users.js
+++ Backend/src/models/users.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -27,7 +28,26 @@ const userSchema = new mongoose.Schema({
       }
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 })
+
+// generate and return a token
+userSchema.methods.generateAuthToken = async function () {
+  const user = this
+  const token = jwt.sign({ _id: user._id.toString() }, 'littlerockstar')
+
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
+
+  return token
+}
 
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email })
--- Backend/src/routers/users.js
+++ Backend/src/routers/users.js
@@ -18,7 +18,8 @@ router.post('/signup', async (req, res) => {
 router.post('/signin', async (req, res) => {
   try {
     const user = await User.findByCredentials(req.body.email, req.body.password)
-    res.send({ user })
+    const token = await user.generateAuthToken()
+    res.send({ user, token })
   } catch (e) {
     res.send(e)
   }
