aff35aee2ca48205a8392169aeba09dd7de750b1
Murari Gupta <alexgupta2367@gmail.com>
2022-04-04 23:56:36 +0545

    created the get contact endpoint to fetch all the contacts at once

--- Backend/src/routers/contacts.js
+++ Backend/src/routers/contacts.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = new express.Router()
 const Contacts = require('../models/contacts')
 
+// creating the POST/contacts endpoint
 router.post('/contacts', async (req, res) => {
   const contact = new Contacts(req.body)
   try {
@@ -9,6 +10,16 @@ router.post('/contacts', async (req, res) => {
     res.status(201).send(contact)
   } catch (e) {
     res.status(400).send(e)
+  }
+})
+
+// creating GET/contacts endpoint to fetch all contacts
+router.get('/contacts', async (req, res) => {
+  try {
+    const contacts = await Contacts.find({})
+    res.send(contacts)
+  } catch (e) {
+    res.status(500).send(e)
   }
 })
 
