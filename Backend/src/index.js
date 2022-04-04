97de5b845a21c6fbcfe684e7b44b8cb201e0ec1d
Murari Gupta <alexgupta2367@gmail.com>
2022-04-04 16:16:38 +0545

    set up the package installed the express

---
+++ Backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('welcome to the home page')
+})
+
+app.listen(port, () => {
+  console.log(`connected to the port ${port}`)
+})
