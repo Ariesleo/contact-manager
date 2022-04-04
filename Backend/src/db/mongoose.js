dcc7b507a7438b8b5d6f013e07164c792a373554
Murari Gupta <alexgupta2367@gmail.com>
2022-04-04 16:25:49 +0545

    adding mongoose and setting a connection

---
+++ Backend/src/db/mongoose.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/contact-manager-api', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  autoIndex: true,
+})
