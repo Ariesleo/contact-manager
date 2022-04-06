a8a0363f390071f041dd7a3d236e43403a4b0515
Murari Gupta <alexgupta2367@gmail.com>
2022-04-06 18:43:40 +0545

    working for the Ui and sending the post request to add the contact

--- frontend/task-manager/src/pages/signIn.js
+++ frontend/task-manager/src/pages/signIn.js
@@ -1,7 +1,19 @@
 
 export const SignIn = () => {
-    return (<div>
-        <h1>SignIn</h1>
+    return (<div class="container" style={{width: '40%'}}>
+        
+        <form>
+            <h1 class="nav justify-content-center">SignIn</h1>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" />
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
     </div>)
 }
 
--- frontend/task-manager/src/pages/signUp.js
+++ frontend/task-manager/src/pages/signUp.js
@@ -1,7 +1,48 @@
+import axios from "axios"
+import { useState } from "react"
+import {Link } from "react-router-dom"
+
 
 export const SignUp = () => {
-    return (<div>
-        <h1>signup</h1>
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const addUser = (e) => {
+        e.preventDefault()
+        const user = {
+            email,
+            password
+        }
+        axios.post(`http://localhost:8000/signup`, user).then(res => {
+            console.log(res)
+            alert(`user with the email: ${email} has been created`)
+        }).catch((e) => {
+            if (e.response.status === 400) {
+                alert("user already created with same email adress")
+            }
+            console.log(e)
+        })
+    }
+
+    return (<div class="container" style={{width: '40%'}}>
+        <form onSubmit={addUser}>
+            <h1 class="nav justify-content-center">SignUp</h1>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={email} onChange={(e) => {
+                    setEmail(e.target.value)
+                }} />
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" value={password} onChange={(e) => {
+                    setPassword(e.target.value)
+                }} />
+            </div>
+            <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                <button type="submit" class="btn btn-primary">Submit</button> 
+                <span>Don't have account? <Link to="/signin">SignIn</Link></span>
+            </div>
+            </form>
     </div>)
 }
-
