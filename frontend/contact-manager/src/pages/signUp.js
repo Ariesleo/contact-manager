f5bbd8e853e8e02431bc69adb5d17916b3a30312
Murari Gupta <alexgupta2367@gmail.com>
2022-05-09 00:24:43 +0545

    removing commented codes

--- frontend/contact-manager/src/pages/signUp.js
+++ frontend/contact-manager/src/pages/signUp.js
@@ -166,53 +166,5 @@ export const SignUp = () => {
         </CardActions>
       </Card>
     </div>
-    // <div class="container" style={{ width: '40%' }}>
-    //   <form onSubmit={addUser}>
-    //     <h1 class="nav justify-content-center">SignUp</h1>
-    //     <div class="mb-3">
-    //       <label for="exampleInputEmail1" class="form-label">
-    //         Email address
-    //       </label>
-    //       <input
-    //         type="email"
-    //         class="form-control"
-    //         id="exampleInputEmail1"
-    //         aria-describedby="emailHelp"
-    //         value={email}
-    //         onChange={(e) => {
-    //           setEmail(e.target.value)
-    //         }}
-    //       />
-    //     </div>
-    //     <div class="mb-3">
-    //       <label for="exampleInputPassword1" class="form-label">
-    //         Password
-    //       </label>
-    //       <input
-    //         type="password"
-    //         class="form-control"
-    //         id="exampleInputPassword1"
-    //         value={password}
-    //         onChange={(e) => {
-    //           setPassword(e.target.value)
-    //         }}
-    //       />
-    //     </div>
-    //     <div
-    //       style={{
-    //         display: 'flex',
-    //         justifyContent: 'space-between',
-    //         alignItems: 'center',
-    //       }}
-    //     >
-    //       <button type="submit" class="btn btn-primary">
-    //         Submit
-    //       </button>
-    //       <span>
-    //         Already have account? <Link to="/signin">SignIn</Link>
-    //       </span>
-    //     </div>
-    //   </form>
-    // </div>
   )
 }
