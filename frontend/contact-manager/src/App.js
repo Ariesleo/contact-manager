8fe98f3eec09f626511297dfb19a058c0c0f425e
Murari Gupta <alexgupta2367@gmail.com>
2022-05-08 10:49:25 +0545

    set up the material ui

--- frontend/contact-manager/src/App.js
+++ frontend/contact-manager/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-// import logo from './logo.svg';
 import './App.css'
 import { Home } from './pages/home'
 import { SignIn } from './pages/signIn'
