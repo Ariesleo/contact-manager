e4d008970e0d770952c0d16cc01aa5d2eb2ea62a
Murari Gupta <alexgupta2367@gmail.com>
2022-05-15 18:12:31 +0545

    created a service layer to control the api request

---
+++ frontend/contact-manager/src/services/api.services.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { getToken } from '../utils/getToken'
+
+const headerData = getToken()
+
+const baseURL = process.env.REACT_APP_URI
+
+export const updateFavourite = async (id) => {
+  return await axios.patch(`${baseURL}/contacts/${id}`)
+}
+
+export const getAllContacts = async (id) => {
+  return await axios.get(`${baseURL}/contacts`, { headers: headerData })
+}
+
+export const deleteContact = async (id) => {
+  return await axios.delete(`${baseURL}/contacts/${id}`, {
+    headers: headerData,
+  })
+}
+
+export const addNewContact = async (newData) => {
+  return await axios.post(`${baseURL}/contacts`, newData, {
+    headers: headerData,
+  })
+}
+
+export const editContact = async (id, data) => {
+  return await axios.put(`${baseURL}/contacts/${id}`, data, {
+    headers: headerData,
+  })
+}
